e7bfe4cf24fafa520c3710e22d61e10e53b16994
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-10-30 23:34:50 +0900

    axiosを使わないため、typescript-axiosからtypescript-fetchに型生成を変更

---
+++ types/typescript-axios/models/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CoffeeHotGet200ResponseInner';
+export * from './CoffeeHotGet200ResponseInnerId';
+export * from './CoffeeHotGet200ResponseInnerIngredients';
+export * from './CoffeeIcedGet200ResponseInner';
