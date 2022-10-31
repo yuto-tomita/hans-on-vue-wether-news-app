ccbcccbe1d25ca207242bac3c9a147cb7abc6374
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-10-31 16:19:30 +0900

    取得したAPIを画面上に表示

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,45 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from '@/components/HelloWorld.vue'
+import { onMounted, computed } from 'vue'
+import { Text } from '@/components/ui'
+import { useFetchApi } from '@/modules/store/useFetchApi'
 
+const fetchApi = useFetchApi()
+
+onMounted(async() => {
+  await Promise.all([
+    fetchApi.fetchHotCoffeeList(),
+    fetchApi.fetchIcedCoffeeList()
+  ])
+})
 </script>
 
 <template>
-  <img
-    alt="Vue logo"
-    src="./assets/logo.png"
-    class="m-auto"
-  >
-  <hello-world
-    msg="Hello tommy boiler template"
-  />
+  <!-- Titleを表示するためのコンポーネントを作成する -->
+  <Text variant="h1">
+    Coffee Enjoy!
+  </Text>
+
+  <div class="flex flex-wrap">
+    <div
+      v-for="hotCoffee in fetchApi.getHotCoffeeList"
+      :key="hotCoffee.id"
+      class="border border-solid border-stone-200 w-1/5 mt-4"
+    >
+      <Text
+        variant="h3"
+        class="align-text-top"
+      >
+        {{ hotCoffee.title }}
+      </Text>
+
+      <img
+        :src="hotCoffee.image"
+        class=" h-56 w-full"
+      >
+    </div>
+  </div>
 </template>
 
 <style>
