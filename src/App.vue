329f44236b8ad5fbb4a9e4b8ebe72b43db925621
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-11-03 17:40:19 +0900

    Cardコンポーネントの反映　

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import { onMounted } from 'vue'
-import { Text } from '@/components/ui'
+import { onMounted, computed } from 'vue'
+import { Text, Card } from '@/components/ui'
 import { useFetchApi } from '@/modules/store/useFetchApi'
+import type { CoffeeHotGet200ResponseInner } from '@/types/typescript-fetch'
 
 const fetchApi = useFetchApi()
 
@@ -13,42 +14,45 @@ onMounted(async() => {
     fetchApi.fetchIcedCoffeeList()
   ])
 })
+
+/** coffeeListのプロパティにアクセスしてvalueを返す */
+const getCoffeeInfoFromApiData = computed(() => {
+  return (
+    // keyの引数は 'title' | 'description' | 'image'のみを許容する
+    key: keyof Pick<CoffeeHotGet200ResponseInner, 'title' | 'description' | 'image'>,
+    index: number
+  ): string => {
+    // null合体演算子 左辺がnull | undefinedである場合に右辺を返す
+    // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing
+    return fetchApi.getHotCoffeeList[index][key] ?? ''
+  }
+})
 </script>
 
 <template>
   <!-- Titleを表示するためのコンポーネントを作成する -->
-  <Text variant="h1">
+  <Text
+    variant="h1"
+    class="text-center mt-2"
+  >
     Coffee Enjoy!
   </Text>
 
-  <div class="flex flex-wrap">
-    <div
-      v-for="hotCoffee in fetchApi.getHotCoffeeList"
-      :key="hotCoffee.id"
-      class="border border-solid border-stone-200 w-1/5 mt-4"
-    >
-      <Text
-        variant="h3"
-        class="align-text-top"
-      >
-        {{ hotCoffee.title }}
-      </Text>
-
-      <img
-        :src="hotCoffee.image"
-        class=" h-56 w-full"
-      >
-    </div>
+  <div class="flex flex-wrap justify-center gap-4 mt-4 pb-10">
+    <!-- 引数を使用しない場合は`_`にすることでエディタからのwarningを回避することができる -->
+    <Card
+      v-for="(_, index) in fetchApi.getHotCoffeeList"
+      :key="index"
+      :img-src="getCoffeeInfoFromApiData('image', index)"
+      :description="getCoffeeInfoFromApiData('description', index)"
+      :title="getCoffeeInfoFromApiData('title', index)"
+    />
   </div>
 </template>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
