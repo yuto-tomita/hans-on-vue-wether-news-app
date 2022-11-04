c81115d9277a31b5e2ad8fb47a85a7a608e2c4af
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-11-04 12:14:15 +0900

    Icedコーヒー一覧を表示できるように加筆

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import { onMounted, computed } from 'vue'
+import { onMounted, computed, ref } from 'vue'
 import { Text, Card } from '@/components/ui'
 import { useFetchApi } from '@/modules/store/useFetchApi'
-import type { CoffeeHotGet200ResponseInner } from '@/types/typescript-fetch'
+
+type HotOrIced = 'HOT' | 'ICED'
+const selectHotOrIced = ref<HotOrIced>('HOT')
 
 const fetchApi = useFetchApi()
 
@@ -15,33 +15,74 @@ onMounted(async() => {
   ])
 })
 
+const switchActiveTab = (CoffeeType: HotOrIced) => {
+  selectHotOrIced.value = CoffeeType
+}
+
+const activeTabClasses = computed(() => {
+  return (coffeeType: HotOrIced) => {
+    return coffeeType === selectHotOrIced.value ? 'font-black' : 'text-slate-200' 
+  }
+})
+
+const coffeeList = computed(() => {
+  return selectHotOrIced.value === 'HOT' ? fetchApi.getHotCoffeeList : fetchApi.getIcedCoffeeList
+})
+
 /** coffeeListのプロパティにアクセスしてvalueを返す */
 const getCoffeeInfoFromApiData = computed(() => {
   return (
     // keyの引数は 'title' | 'description' | 'image'のみを許容する
-    key: keyof Pick<CoffeeHotGet200ResponseInner, 'title' | 'description' | 'image'>,
+    key:'title' | 'description' | 'image',
     index: number
   ): string => {
-    // null合体演算子 左辺がnull | undefinedである場合に右辺を返す
-    // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing
-    return fetchApi.getHotCoffeeList[index][key] ?? ''
+    if (selectHotOrIced.value === 'HOT') {
+      // null合体演算子 左辺がnull | undefinedである場合に右辺を返す
+      // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing
+      return fetchApi.getHotCoffeeList[index][key] ?? ''
+    } else {
+      return fetchApi.getIcedCoffeeList[index][key] ?? ''
+    }
   }
 })
 </script>
 
 <template>
   <!-- Titleを表示するためのコンポーネントを作成する -->
-  <Text
-    variant="h1"
-    class="text-center mt-2"
-  >
-    Coffee Enjoy!
-  </Text>
+  <div class="text-center mt-2">
+    <Text variant="h1">
+      Coffee Enjoy!
+    </Text>
+  
+    <!-- タブ切り分け機能は最初は実装しない。lintエラー解消の次のタイミングで実装する -->
+    <div>
+      <span
+        :class="activeTabClasses('HOT')"
+        class="text-lg cursor-pointer"
+        @click="switchActiveTab('HOT')"
+      >
+        Hot
+      </span>
+      |
+      <span
+        :class="activeTabClasses('ICED')"
+        class="text-lg cursor-pointer"
+        @click="switchActiveTab('ICED')"
+      >
+        Iced
+      </span>
+    </div>
+  </div>
 
   <div class="flex flex-wrap justify-center gap-4 mt-4 pb-10">
     <!-- 引数を使用しない場合は`_`にすることでエディタからのwarningを回避することができる -->
+    <!-- 勉強会手順1: まずは普通にHTMLにベタ書きする -->
+    <!-- 勉強会手順2: コンポーネントに分割してみる -->
+    <!-- 勉強会手順3: 発生するlintエラーを解消する -->
+    <!-- 勉強会手順4: タブ実装 -->
+    <!-- 勉強会手順4: 返すListをHot, Icedに変更できるようにする -->
     <Card
-      v-for="(_, index) in fetchApi.getHotCoffeeList"
+      v-for="(_, index) in coffeeList"
       :key="index"
       :img-src="getCoffeeInfoFromApiData('image', index)"
       :description="getCoffeeInfoFromApiData('description', index)"
